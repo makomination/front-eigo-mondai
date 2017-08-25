caeda8c23d49ac642bfb01b771be0cd22d822080
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2017-08-24 21:26:55 -0700

    separate codes into another component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
