6a684e8b603a30ffcf75949274b6e8624e4df53a
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2017-08-24 18:16:01 -0700

    selecred hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,40 @@
 import { Component } from '@angular/core';
 import { Hero } from "./hero";
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
   title: string;
   heroes: Array<Hero>;
   myHero: Hero | null;
+  selectedHero: Hero | null;
 
   constructor(){
     this.title = 'Tour of Heroes';
-    this.heroes = [
-      new Hero(1, 'Makoto'),
-      new Hero(2, 'Windstorm'),
-      new Hero(3, 'Bombasto'),
-      new Hero(4, 'Magneta'),
-      new Hero(5, 'Tornado')
-    ];
+    this.heroes = HEROES;
     this.myHero = this.heroes[0];
-  } 
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
