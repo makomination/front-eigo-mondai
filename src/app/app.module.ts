6d99b2179de13f51abb34df2815e4d71945cd061
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2017-08-25 10:23:11 -0700

    up to service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
-
 
 @NgModule({
   declarations: [
