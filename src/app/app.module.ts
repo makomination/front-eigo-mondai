fc75123d9cb40dc529b5a7f846a3bad3d2db9661
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2017-09-02 09:33:49 -0700

    install angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule }     from './app-routing.module';
+import { HttpModule }    from '@angular/http';
 
 import { HeroService } from './hero.service';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { DashboardComponent } from './dashboard.component'
 import { AppComponent } from './app.component';
@@ -19,7 +24,9 @@ import { HeroesComponent }     from './heroes.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule, // <-- import the FormsModule before binding with [(ngModel)] 
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    InMemoryWebApiModule.forRoot(InMemoryDataService),    
+    HttpModule,    
     AppRoutingModule
   ],
   providers: [HeroService],
